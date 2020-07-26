ff77e886fd243664f6cb621120a1905109882ebf
Michael <30936296+mtan93@users.noreply.github.com>
2020-07-26 13:19:39 +0100

    Create mac.sh

---
+++ mac.sh
@@ -0,0 +1,3 @@
+sudo killall "Google Drive File Stream"
+sudo kextload "/Applications/Google Drive File Stream.app/Contents/MacOS/dfsfuse.kext"
+1>/dev/null 2>/dev/null /Applications/Google\ Drive\ File\ Stream.app/Contents/MacOS/Google\ Drive\ File\ Stream &
